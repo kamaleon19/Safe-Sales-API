e9c9aa642714e9612deb8af708d31eed560b9631
kamaleon19 <joaco.px@gmail.com>
2025-01-28 18:38:04 -0300

    localhost adherido en CORS

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: [
-        'http://localhost:3000',
+        'http://localhost:**',
         'https://ignaciomontaldi.github.io/front-safe-sales/'
       ], 
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
