802e9097d6d19f3296a980a3cd8fe2796c06903e
kamaleon19 <joaco.px@gmail.com>
2025-01-29 16:23:32 -0300

    metodo ping-pong agregado

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiOperation({ summary: 'ping-pong: Determina que la API este funcionando correctamente' })
+  @ApiResponse({ status: 200, description: 'pong' })
+  @Get('ping')
+  ping(): string {
+    return this.appService.pong();
   }
 }
